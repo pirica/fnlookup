f7fe8557b324cd66c530c04cf21944cadf579c73
tposejank <81495861+tposejank@users.noreply.github.com>
2022-12-20 17:05:11 -0500

    updates

---
+++ js/backend.js
@@ -0,0 +1,5 @@
+localStorage.keyFNAPI = '1eea611e-bed8-4d3b-aa22-aae0b15f1c08'
+localStorage.keyFNAPIIo = '46c8d137-adea4f05-5928e673-ffdcf29d'
+if (localStorage.requestLanguage === undefined) {
+    localStorage.requestLanguage = 'en';
+}
