e87b48665fb8a1fbabb09c5fb84d2350a8c65027
tposejank <81495861+tposejank@users.noreply.github.com>
2022-12-21 20:29:50 -0500

    language change support

--- js/backend.js
+++ js/backend.js
@@ -1,12 +1,20 @@
 localStorage.keyFNAPI = '1eea611e-bed8-4d3b-aa22-aae0b15f1c08'
 localStorage.keyFNAPIIo = '46c8d137-adea4f05-5928e673-ffdcf29d'
-if (localStorage.requestLanguage === undefined) {
-    localStorage.requestLanguage = 'en';
-}
 
-supportedLanguages = [
+window.supportedLanguages = [
     'en', 'ar', 'de', 'es', 'es-419', 'fr', 'it', 'ja', 'ko', 'pl', 'pt-BR', 'ru', 'tr', 'zh-CN', 'zh-Hant'
 ];
+
+if (localStorage.requestLanguage === undefined) {
+    if (window.supportedLanguages.includes(window.navigator.language)) {
+        localStorage.requestLanguage = window.navigator.language;
+    }
+}
+
+function switchLanguage(to) {
+    localStorage.requestLanguage = to;
+    document.location.reload();
+}
 
 function geturllang(url, type) {
     let t = '?';
